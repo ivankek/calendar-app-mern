bf58d8c92f384402d5a23b3f9d0e8994f1c80bce
Ivan Kek <ivan.kek@hotmail.com>
2022-03-23 13:28:02 -0300

    modal added

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { CalendarApp } from "./CalendarApp";
+import "./styles.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <CalendarApp />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+ReactDOM.render(<CalendarApp />, document.getElementById("root"));
