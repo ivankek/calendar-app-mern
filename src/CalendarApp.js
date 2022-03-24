800261cbb30190dbc3cff91c2c3acbe743f336c2
Ivan Kek <ivan.kek@hotmail.com>
2022-03-24 00:22:30 -0300

    redux configuration added

--- src/CalendarApp.js
+++ src/CalendarApp.js
@@ -1,12 +1,12 @@
 import React from "react";
+import { Provider } from "react-redux";
 import { AppRouter } from "./routers/AppRouter";
+import { store } from "./store/store";
 
 export const CalendarApp = () => {
   return (
-    <div>
-      <h1>
-        <AppRouter />
-      </h1>
-    </div>
+    <Provider store={store}>
+      <AppRouter />
+    </Provider>
   );
 };
